97ba92d5a9d02b50e6682892f396c0decf9125d0
benprice2 <ben.price700@gmail.com>
2023-09-20 17:17:49 +1200

    working weather api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './css/App.css';
 import Weather_App from './components/Weather_App';
-const api_key = "85cffdf71c5e0e3b174f2146f103b5a3";
-const api_url = "https://api.openweathermap.org/data/2.5/weather?q=" //{city name}&appid={API key}
+
 
 
 function App() {
--- src/components/Weather_App.js
+++ src/components/Weather_App.js
@@ -8,28 +8,54 @@ import snow from "../assets/images/snow.png"
 import wind from "../assets/images/wind.png"
 
 export const Weather_App = () => {
+    
+    const apiKey = "85cffdf71c5e0e3b174f2146f103b5a3";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+    const searchBox = document.querySelector(".search input")
+    const searchBtn = document.querySelector(".search button")
+
+
+    async function checkWeather(cityName){
+        const response = await fetch(apiUrl + cityName +`&appid=${apiKey}`);
+        var data = await response.json();
+
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    }
+    searchBtn.addEventListener("click", () => {
+        checkWeather(searchBox.value);
+    })
+
+
+
     return(
-        <div class="card">
-            <div class="search">
+        <div className="card">
+            <div className="search">
                 <input type="text" placeholder="Enter city name" spellCheck="false"/>
                 <button><img src={search_icon} alt="search_icon"/></button>
             </div>
-            <div class="weather">
+            <div className="weather">
                 <img src={wind} alt="weather_image" class="weather-icon"/>
-                <h1 class="temp">22°c</h1>
-                <h2 class="city">New York</h2>
-                <div class="details">
-                    <div class="col">'
+                <h1 className="temp">22°C</h1>
+                <h2 className="city">New York</h2>
+                <div className="details">
+                    <div className="col">'
                         <img src={humidity} alt="humidty-img"/>
                         <div>
                             <p class="humidity">50%</p>
                             <p>Humidity</p>
                         </div>
                     </div>
-                    <div class="col">'
+                    <div className="col">'
                         <img src={wind} alt="wind-img"/>
                         <div>
-                            <p class="wind">15 km/h</p>
+                            <p className="wind">15km/h</p>
                             <p>Wind Speed</p>
                         </div>
                     </div>
